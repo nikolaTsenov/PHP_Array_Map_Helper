05ed2b8ddbcdc489237e06b90c3fd37d4b8d4c7c
nikolaTsenov <niki_tsenov@abv.bg>
2017-12-18 00:17:29 +0200

    MAP-1 added function that gets multiple assoc values by mapping them to multiple key value pairs from the same level

--- src/ArrayMapHelper.php
+++ src/ArrayMapHelper.php
@@ -24,16 +24,40 @@ class ArrayMapHelper
 		$this->default = $defaultValue;
 	}
 	
+	/**
+	 * Gets private $searchedValue
+	 * 
+	 * @author nikola.tsenov
+	 * 
+	 * @return unknown
+	 */
 	public function getSearchedValue()
 	{
 		return $this->searchedValue;
 	}
 	
+	/**
+	 * Sets private $searchedValue
+	 * 
+	 * @author nikola.tsenov
+	 */
 	public function setSearchedValue($value)
 	{
 		$this->searchedValue = $value;
 	}
 	
+	/**
+	 * Creates object of self and returns 'getAssocValueByMappingRecursion'.
+	 * 
+	 * @author nikola.tsenov
+	 * 
+	 * @param array $haystack
+	 * @param int|string $mapKey
+	 * @param unknown $mapValue
+	 * @param int|string $searchedKey
+	 * @param unknown $defaultValue
+	 * @return unknown
+	 */
 	public static function getAssocValueByMapping($haystack, $mapKey, $mapValue, $searchedKey, $defaultValue = false)
 	{
 		$arrayMapHelper = new static($haystack, $defaultValue);
@@ -41,6 +65,18 @@ class ArrayMapHelper
 		return self::getAssocValueByMappingRecursion($arrayMapHelper, $arrayMapHelper->array, $mapKey, $mapValue, $searchedKey);
 	}
 	
+	/**
+	 * Gets a key's value by mapping it to a key-value pair from the same level in multidimensional array.
+	 * 
+	 * @author nikola.tsenov
+	 * 
+	 * @param ArrayMapHelper $arrayMapHelper
+	 * @param array $haystack
+	 * @param int|string $mapKey
+	 * @param unknown $mapValue
+	 * @param int|string $searchedKey
+	 * @return unknown
+	 */
 	protected static function getAssocValueByMappingRecursion(ArrayMapHelper $arrayMapHelper, $haystack, $mapKey, $mapValue, $searchedKey)
 	{
 		if (isset($haystack[$mapKey]) && $haystack[$mapKey] == $mapValue && array_key_exists($searchedKey, $haystack)) {
@@ -53,6 +89,76 @@ class ArrayMapHelper
 			}
 			if (is_array($value)) {
 				self::getAssocValueByMappingRecursion($arrayMapHelper, $value, $mapKey, $mapValue, $searchedKey);
+			}
+		}
+	
+		return $arrayMapHelper->getSearchedValue() ?? $arrayMapHelper->default;
+	}
+	
+	/**
+	 * Creates object of self and returns 'getAssocValueByMultiMappingRecursion'.
+	 *
+	 * @author nikola.tsenov
+	 *
+	 * @param array $haystack
+	 * @param array $mapArray
+	 * @param array $searchedKeysArray
+	 * @param bool $assoc - result will be assoc array with $searchedKeysArray as keys
+	 * @param unknown $defaultValue
+	 * @return unknown $result
+	 */
+	public static function getAssocValuesByMultiMapping($haystack, array $mapArray, array $searchedKeysArray, $assoc = false, $defaultValue = false)
+	{
+		$arrayMapHelper = new static($haystack, $defaultValue);
+	
+		$result = self::getAssocValuesByMultiMappingRecursion($arrayMapHelper, $arrayMapHelper->array, $mapArray, $searchedKeysArray);
+		
+		if (! $assoc && ! empty($result) && is_array($result)) {
+			return array_values($result);
+		}
+		
+		return $result;
+	}
+	
+	/**
+	 * Gets array of searched keys values by mapping them to key-value pairs from the same level in multidimensional array.
+	 *
+	 * @author nikola.tsenov
+	 *
+	 * @param array $haystack
+	 * @param array $mapArray
+	 * @param array $searchedKeysArray
+	 * @param unknown $defaultValue
+	 * @return unknown
+	 */
+	protected static function getAssocValuesByMultiMappingRecursion(ArrayMapHelper $arrayMapHelper, $haystack, $mapArray, $searchedKeysArray)
+	{
+		$bottom = true;
+		foreach ($mapArray AS $mapKey => $mapValue) {
+			if (! isset($haystack[$mapKey]) || $haystack[$mapKey] != $mapValue) {
+				$bottom = false;
+				break;
+			}
+		}
+		
+		if ($bottom) {
+			$foundArray = [];
+			foreach ($searchedKeysArray AS $foundKey) {
+				if (array_key_exists($foundKey, $haystack)) {
+					$foundArray[$foundKey] = $haystack[$foundKey];
+				}
+			}
+			if (! empty($foundArray)) {
+				$arrayMapHelper->setSearchedValue($foundArray);
+			}
+		}
+	
+		foreach ($haystack AS $key => $value) {
+			if (! is_null($arrayMapHelper->getSearchedValue())) {
+				break;
+			}
+			if (is_array($value)) {
+				self::getAssocValuesByMultiMappingRecursion($arrayMapHelper, $value, $mapArray, $searchedKeysArray);
 			}
 		}
 	
--- test/scriptForTesting.php
+++ test/scriptForTesting.php
@@ -31,7 +31,8 @@ function dumpAndDieScript()
 try {
 	//include example array
 	include_once './exampleArray.php';
-
+	
+	//getAssocValueByMapping
 	$searchValue = ArrayMapHelper::getAssocValueByMapping($array, 'subjectname', 'Law issues', 'tension');
 	$searchValue2 = ArrayMapHelper::getAssocValueByMapping($array, 'materialid', 'adsasd-asdasd-asdasd-asdasd', 'materialsubject');
 	$searchValue3 = ArrayMapHelper::getAssocValueByMapping($array, 'materialid', 'adsasd-asdasd-asdasd-asdasd', 'materialtype');
@@ -40,8 +41,37 @@ try {
 	$searchValue6 = ArrayMapHelper::getAssocValueByMapping($array, 'subjectid', 2, 'tension');
 	$searchValue7 = ArrayMapHelper::getAssocValueByMapping($array, 0, '', 'materialsubject');
 	$searchValue8 = ArrayMapHelper::getAssocValueByMapping($array, 0, '', 'materialsubject', 'default');
+	
+	//getAssocValuesByMultiMapping
+	$searchValue9 = ArrayMapHelper::getAssocValuesByMultiMapping($array, [
+		'materialid' => 'kjashfbkasdjbffka-adhksfbdlas-dasfdasf-asdfdfas',
+		'materialtype' => 'pp presentation',
+	], ['materialsubject']);
+	$searchValue10 = ArrayMapHelper::getAssocValuesByMultiMapping($array, [
+			'subjectid' => 2,
+			'subjectname' => 'New business investments',
+	], ['tension', 'materials'], true);
+	$searchValue11 = ArrayMapHelper::getAssocValuesByMultiMapping($array, [
+			'version' => '2.0',
+	], [0]);
+	$searchValue12 = ArrayMapHelper::getAssocValuesByMultiMapping($array, [
+			'version' => '2.0',
+	], [0], false, 'default');
 
-	dumpAndDieScript($searchValue, $searchValue2, $searchValue3, $searchValue4, $searchValue5, $searchValue6, $searchValue7, $searchValue8);
+	dumpAndDieScript(
+		$searchValue, 
+		$searchValue2, 
+		$searchValue3, 
+		$searchValue4, 
+		$searchValue5, 
+		$searchValue6, 
+		$searchValue7, 
+		$searchValue8,
+		$searchValue9,
+		$searchValue10,
+		$searchValue11,
+		$searchValue12
+	);
 } catch(ArrayMapException $ex) {
 	$erMsg = $ex->getMessage();
 	echo $erMsg;
